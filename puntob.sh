0756509b5af1492a7ebc809ef10076e47275d64e
JosueDamacio <damaciojosue@gmail.com>
2024-11-22 03:45:35 +0000

    punto b guardado

---
+++ puntob.sh
@@ -0,0 +1,9 @@
+   47  ssh-keygen
+   48  ll .ssh/
+   49  ll .ssh/authorized_keys 
+   50  cat .ssh/id_rsa.pub
+   51  ssh vagrant@192.168.56.2
+   79  ansible-playbook -i inventory playbook.yml
+   80  apt install ansible
+   81  sudo apt install ansible
+   82  ansible-playbook -i inventory playbook.yml
